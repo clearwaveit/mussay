86deca959898728f29ff970431f77d8a5b93335a
clearwaveit <tanveer@cwit.ae>
2026-05-22 16:09:06 +0400

    Fix: skip prisma generate on startup to avoid permission error

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Running database migrations..."
-prisma db push --schema ./prisma/schema.prisma
+prisma db push --schema ./prisma/schema.prisma --skip-generate
 echo "Starting application..."
 node server.js
